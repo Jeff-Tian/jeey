6c5737356d80e9fb99b850f5026d2c337460107a
Jeff Tian <jeff.tian@outlook.com>
2019-10-10 16:17:11 +0800

    feat: add try/catch to start client script

--- start-client.js
+++ start-client.js
@@ -1,3 +1,10 @@
-const args = [ 'start' ];
+const args = ['start'];
 const opts = { stdio: 'inherit', cwd: 'client', shell: true };
-require('child_process').spawn('npm', args, opts);
+
+try {
+    require('child_process').spawn('npm', args, opts)
+    console.log('客户端已经启动')
+} catch (ex) {
+    console.error('碰到错误：', ex)
+    process.exit(1)
+}
